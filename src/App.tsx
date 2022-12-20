b735dd33234107ecd503c7759e41ff42f5d3d6dc
Bartek Kwapisz <kwapiszbartek@gmail.com>
2022-12-20 01:10:10 +0100

    basic css layout + music

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,36 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+function App() { 
+  const [audio, setAudio] = useState(new Audio("src/assets/candy.mp3"));
+  const [music, setMusic] = useState(true);
+  const playMusic = () => {
+    if (music){
+      audio.play(); 
+    } else {
+      audio.pause(); 
+    }
+    setMusic(music => !music);
+  };
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="grid-container">
+        <div className="grid-item">Eat</div>
+        <div className="grid-item image" onClick={playMusic}>
+          <img src="https://media.giphy.com/media/eZsKqkZUEM5vG/giphy.gif" />
+        </div>
+        <div className="grid-item">Fun</div>
+        <div className="grid-item">Sleep</div>
+        <div className="grid-item">Energy</div>
+        <div className="grid-item">Dress</div>
+        <div className="grid-item">Toilet</div>
+        <div className="grid-item">Play</div>
+        <div className="grid-item">&lt;&lt; Your choice &gt;&gt;</div>
+        <div className="grid-item">Use toilet!</div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
